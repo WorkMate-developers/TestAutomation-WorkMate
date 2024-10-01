b4fa0c092339a1da38520ba58b2eba35e258cab4
Stefan <Stefan@PC-13>
2024-10-01 21:30:20 +0200

    WM-25 Dodao logovanje za Test Automation framework.

--- src/test/java/api/CitiesBasicTest.java
+++ src/test/java/api/CitiesBasicTest.java
@@ -3,23 +3,37 @@ package api;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 
+/**
+ * This is a basic test that verifies whether the response code for GET /cities is 200. It also prints the response.
+ */
+
 public class CitiesBasicTest {
+
+    private static final Logger log = LoggerFactory.getLogger(CitiesBasicTest.class);
+    private final String testName = this.getClass().getName();
 
     @Test(groups = {"regression"})
     @Parameters({"backendServerUrl"})
     public void getCities(String backendServerUrl) {
 
+        log.info("Starting test: " + testName);
+
         RequestSpecification requestSpecification = new RequestSpecBuilder()
                 .setBaseUri(Endpoints.HTTP + backendServerUrl + Endpoints.API)
-                .build().log().all();
+                .build();
 
         Response response = given().spec(requestSpecification).get(Endpoints.CITIES);
 
-        response.getBody().prettyPrint();
+        log.debug("Actual response:\n" + response.getBody().asPrettyString());
+
+        assert response.getStatusCode() == 200 : "Invalid response code! Actual response code: " + response.getStatusCode();
+
     }
 }
