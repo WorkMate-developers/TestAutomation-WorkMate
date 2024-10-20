76ee389d1d511a7f9e9d10e9f1f5820d276362e0
Stefan <Stefan@PC-13>
2024-10-20 11:53:08 +0200

    Dodao WebDriverUtils.java, za driver handling.

---
+++ src/main/java/utils/WebDriverUtils.java
@@ -0,0 +1,69 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.Assert;
+
+public class WebDriverUtils extends LoggerUtils {
+
+    /**
+     * Sets up Web Driver based on preferences picked up from common.properties.
+     *
+     * @return WebDriver
+     */
+
+    public static WebDriver setUpDriver() {
+        WebDriver driver = null;
+
+        String sBrowser = PropertiesUtils.getBrowser();
+        boolean bHeadless = PropertiesUtils.getBoolProperty("headless");
+
+        log.debug("Set up WebDriver: " + sBrowser + ", headless=" + bHeadless);
+
+        try {
+            switch (sBrowser) {
+                case "chrome" -> {
+                    ChromeOptions chromeOptions = new ChromeOptions();
+                    if (bHeadless) {
+                        chromeOptions.addArguments("--headless");
+                    }
+                    driver = new ChromeDriver(chromeOptions);
+                }
+                case "firefox" -> {
+                    FirefoxOptions firefoxOptions = new FirefoxOptions();
+                    if (bHeadless) {
+                        firefoxOptions.addArguments("--headless");
+                    }
+                    driver = new FirefoxDriver(firefoxOptions);
+                }
+                case "edge" -> {
+                    EdgeOptions edgeOptions = new EdgeOptions();
+                    if (bHeadless) {
+                        edgeOptions.addArguments("--headless");
+                    }
+                    driver = new EdgeDriver(edgeOptions);
+                }
+                default -> Assert.fail("Cannot create driver, " + sBrowser + " is not recognized as valid browser!");
+            }
+        } catch (Exception e) {
+            Assert.fail("Exception thrown while creating driver! Message: " + e.getMessage());
+        }
+
+        if (PropertiesUtils.getBoolProperty("maximized")) {
+            driver.manage().window().maximize();
+        }
+
+        return driver;
+    }
+
+    public static void quitDriver(WebDriver driver) {
+        log.debug("Quitting driver. Bye!");
+        driver.quit();
+    }
+
+}
